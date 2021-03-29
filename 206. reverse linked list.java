53369e2270b5f806e79e42d0147a2b0d4e67a2f0
DouLiu <LDLucien@hotmail.com>
2021-03-29 17:16:22 +0200

    reverse linked list

---
+++ 206. reverse linked list.java
@@ -0,0 +1,32 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+// 思路: change the current node's next pointer to point the previous element. remember to store 
+// the previous element, since the ListNode do not sotre that, and also need another pointer to
+// sotre the next node before changing the reference.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        
+        while (head!=null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
